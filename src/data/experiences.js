d57e12204416d2e10db4b1664e48a16f4e8955e9
ice-009 <21bma009@nith.ac.in>
2023-03-31 12:27:56 +0530

    first commit

---
+++ src/data/experiences.js
@@ -0,0 +1,45 @@
+import geometic from "../assets/images/geometic.jpeg";
+import draic from "../assets/images/image2.png";
+import iste from "../assets/images/Iste.webp";
+import matcom from "../assets/images/matcom.jpeg";
+
+import { blue, green, pink, yellow } from "../utils";
+
+export const experiences = [
+  {
+    logo: matcom,
+    name: "MATCOM",
+    joined: "Dec'22",
+    end: "Present",
+    title: "Member",
+    bio: "Building projects for college's Annual TechFest Nimbus, made Team's website, wrote a Research Paper and organised a WorkShop on mAchine Learning and Artificial Intelligence.",
+    color: blue,
+  },
+  {
+    logo: iste,
+    name: "ISTE",
+    joined: "Jan'22",
+    end: "Present",
+    title: "Executive Member",
+    bio: "Building various projects for the Society, organised different events like Hult Prize, Prodyogyiki, Annual Convention etc.  ",
+    color: green,
+  },
+  {
+    logo: geometic,
+    name: "Geometic EduServices Ltd.",
+    joined: "Dec'21",
+    end: "Mar'22",
+    title: "Subject Matter Expert Intern",
+    bio: "Taught Physics and Mathematics to JEE students and cleared their doubts.",
+    color: yellow,
+  },
+  // {
+  //   logo: draic,
+  //   name: "DRAIC",
+  //   joined: "Sept'19",
+  //   end: "Jan'22",
+  //   title: "Team Member",
+  //   bio: "In D.Y. Patil Robotics and A.I. club we developed collaborative skills. Built amazing projects and brainstormed on various problem statements",
+  //   color: pink,
+  // },
+];
